5bdeae317b7ea45e68c171d581faab5710cd94e9
jaro <jarosz.bartlomiej89@gmail.com>
2019-03-28 11:43:11 +0100

    Optimized mousemove event

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,22 @@
 const pointer = document.getElementById('pointer');
 
+function throttled(delay, fn) {
+  let lastCall = 0;
+  return function (...args) {
+    const now = (new Date).getTime();
+    if (now - lastCall < delay) {
+      return;
+    }
+    lastCall = now;
+    return fn(...args);
+  }
+}
+
 const pointerMove = (e) => {
   pointer.style.left = `${e.clientX}px`;
   pointer.style.top = `${e.clientY}px`;
 };
+const optimizedPointerMove = throttled(70, pointerMove);
 
 if (matchMedia) {
 const mq = window.matchMedia("(min-width: 1200px)");
@@ -13,8 +26,8 @@ WidthChange(mq);
 
 function WidthChange(mq) {
   if (mq.matches) {
-    document.addEventListener('mousemove', pointerMove)
+    document.addEventListener('mousemove', optimizedPointerMove)
   } else {
-    document.removeEventListener('mousemove', pointerMove)
+    document.removeEventListener('mousemove', optimizedPointerMove)
   }
 }
